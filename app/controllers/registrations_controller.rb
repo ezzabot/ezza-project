ed43afd1023136a6076d2d061ec27db54efc3040
ezzabot <ezzawa1324@gmail.com>
2021-03-13 12:39:29 -1200

    fix: username

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,10 @@
+class RegistrationsController < Devise::RegistrationsController
+    # before_action :set_product, only: %i[ show edit update destroy ]
+    
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
