92be7f1f5c34bf364e7c24eac7ceddd4dea23571
ezzabot <ezzawa1324@gmail.com>
2021-03-17 08:34:22 -1200

    add: Boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import "channels"
 
 Rails.start()
